71a5c0248faea21210395164beb501d0c3e1e678
José Anderson Custódio <anderson.k1006@gmail.com>
2024-03-26 20:46:11 -0300

    init commit

---
+++ src/styles/styles.js
@@ -0,0 +1,8 @@
+// Escreva varíaveis que serão importadas em outros arquivos de estilos
+export const colors = {
+    background: '#282c34',
+    text: '#FFFFFF',
+    primary: '#61dafb',
+    error: '#FF0000',
+    sucess: '#00FF00',
+};
